3eb970d39aa23d88cbe3ef1d424db0104c57cef5
JUKI-J <jju.ki@hotmail.com>
2021-11-04 23:25:00 +0900

    Initial commit

---
+++ src/main/java/com/example/springgradlebook/SpringGradleBookApplication.java
@@ -0,0 +1,13 @@
+package com.example.springgradlebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGradleBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringGradleBookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springgradlebook/SpringGradleBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springgradlebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGradleBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
